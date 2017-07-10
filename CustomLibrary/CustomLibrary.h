f8bd37c2e75af89b5bc1e085a43c0d6b647de3d2
Venugopal Reddy Seelam <svenugopalreddy@inlmac01s-mac-mini.local>
2017-07-10 17:33:55 +0530

    Initial Commit

---
+++ CustomLibrary/CustomLibrary.h
@@ -0,0 +1,19 @@
+//
+//  CustomLibrary.h
+//  CustomLibrary
+//
+//  Created by Venugopal Reddy Seelam on 7/10/17.
+//  Copyright © 2017 Inlogic Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomLibrary.
+FOUNDATION_EXPORT double CustomLibraryVersionNumber;
+
+//! Project version string for CustomLibrary.
+FOUNDATION_EXPORT const unsigned char CustomLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomLibrary/PublicHeader.h>
+
+
---
+++ CustomLibrary/Sample.swift
@@ -0,0 +1,15 @@
+//
+//  Sample.swift
+//  CustomLibrary
+//
+//  Created by Venugopal Reddy Seelam on 7/10/17.
+//  Copyright © 2017 Inlogic Technologies. All rights reserved.
+//
+
+import Foundation
+
+public class Logging{
+    public class func welcome() ->Void{
+        print("welcome....")
+    }
+}
